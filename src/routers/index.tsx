8c17d678a63d2135261af0cd8a8018593fb5a36a
RickDan <tangzhiming90@gmail.com>
2023-03-04 15:30:07 +0800

    修改登录页和loading样式

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { useRoutes, Navigate, RouteObject } from "react-router-dom";
-import lazyLoad from "./lazyload";
-// import { NotFound } from "@/components/ErrorMessage/404";
-// import { LayoutIndex } from "@/layouts";
-// import { Login } from "@/views/login";
-// import { Home } from "@/views/home";
-// import { DataScreen } from "@/views/DataScreen";
-// import { UseHook } from "@/views/proTable/useHook";
-// import { UseComponent } from "@/views/proTable/useComponent";
-// import { DataVisualize } from "@/views/dashboard/dataVisualize";
+import lazyload from "./lazyload";
+import Login from "@/views/login/index";
+import LayoutIndex from "@/layouts/index";
 
 const rootRouter: RouteObject[] = [
 	{
@@ -17,44 +11,47 @@ const rootRouter: RouteObject[] = [
 	},
 	{
 		path: "/login",
-		element: lazyLoad(React.lazy(() => import("@/views/login/index")))
+		element: <Login />
+		// element: lazyLoad(React.lazy(() => import("@/views/login/index"))),
 	},
 	{
-		element: lazyLoad(React.lazy(() => import("@/layouts/index"))),
+		element: <LayoutIndex />,
+		// element: lazyLoad(React.lazy(() => import("@/layouts/index"))),
 		children: [
 			{
 				path: "/home",
-				element: lazyLoad(React.lazy(() => import("@/views/home/index")))
+				element: lazyload(React.lazy(() => import("@/views/home/index")))
 			},
 			{
 				path: "/dataScreen",
-				element: lazyLoad(React.lazy(() => import("@/views/DataScreen/index")))
+				element: lazyload(React.lazy(() => import("@/views/dataScreen/index")))
 			},
 			{
 				path: "/proTable/useHooks",
-				element: lazyLoad(React.lazy(() => import("@/views/proTable/useHook/index")))
+				element: lazyload(React.lazy(() => import("@/views/proTable/useHook/index")))
 			},
 			{
 				path: "/proTable/useComponent",
-				element: lazyLoad(React.lazy(() => import("@/views/proTable/useComponent/index")))
+				element: lazyload(React.lazy(() => import("@/views/proTable/useComponent/index")))
 			},
+
 			{
 				path: "/dashboard/dataVisualize",
-				element: lazyLoad(React.lazy(() => import("@/views/dashboard/dataVisualize/index")))
+				element: lazyload(React.lazy(() => import("@/views/dashboard/dataVisualize/index")))
 			}
 		]
 	},
 	{
 		path: "/403",
-		element: lazyLoad(React.lazy(() => import("@/components/ErrorMessage/403")))
+		element: lazyload(React.lazy(() => import("@/components/ErrorMessage/403")))
 	},
 	{
 		path: "/500",
-		element: lazyLoad(React.lazy(() => import("@/components/ErrorMessage/500")))
+		element: lazyload(React.lazy(() => import("@/components/ErrorMessage/500")))
 	},
 	{
 		path: "*",
-		element: lazyLoad(React.lazy(() => import("@/components/ErrorMessage/404")))
+		element: lazyload(React.lazy(() => import("@/components/ErrorMessage/404")))
 	}
 ];
 
@@ -62,4 +59,5 @@ const Router = () => {
 	const routes = useRoutes(rootRouter);
 	return routes;
 };
+
 export default Router;
