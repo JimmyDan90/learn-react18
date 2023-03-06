7fdd355844e4ad300c2a7fc74b55306ca2b195f1
RickDan <tangzhiming90@gmail.com>
2023-03-06 09:43:44 +0800

    完成浏览器全屏

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -21,3 +21,11 @@ declare interface ViteEnv {
 	VITE_BUILD_GZIP: boolean;
 	VITE_REPORT: boolean;
 }
+
+declare interface MenuInfo {
+	key: string;
+	keyPath: string[];
+	/** @deprecated This will not support in future. You should avoid to use this */
+	item: React.ReactInstance;
+	domEvent: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>;
+}
