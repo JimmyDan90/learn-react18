e90335e4d37db4fbf98a26bee2b8b5b77d01455a
RickDan <tangzhiming90@gmail.com>
2023-03-06 16:01:42 +0800

    新增按钮权限hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "@/styles/reset.less";
-import "@/styles/common.less";
 import "@/assets/iconfont/iconfont.less";
 import "antd/dist/antd.css";
 import "@/language/index";
+import "@/styles/common.less";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { store, persistor } from "@/redux";
