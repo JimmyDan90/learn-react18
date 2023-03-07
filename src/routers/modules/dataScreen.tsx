416a15b5f4351966aca8adb81c5ff261f7665f68
RickDan <tangzhiming90@gmail.com>
2023-03-07 14:35:23 +0800

    新增数据大屏模块

--- src/routers/modules/dataScreen.tsx
+++ src/routers/modules/dataScreen.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-import lazyload from "@/routers/utils/lazyload";
-import { LayoutIndex } from "@/routers/constant";
+import lazyLoad from "@/routers/utils/lazyload";
 import { RouteObject } from "@/routers/interface";
 
 // 数据大屏模块
 const dataScreenRouter: Array<RouteObject> = [
 	{
-		element: <LayoutIndex />,
-		children: [
-			{
-				path: "/dataScreen/index",
-				element: lazyload(React.lazy(() => import("@/views/dataScreen/index"))),
-				meta: {
-					requiresAuth: true,
-					title: "数据大屏",
-					key: "dataScreen"
-				}
-			}
-		]
+		path: "/dataScreen/index",
+		element: lazyLoad(React.lazy(() => import("@/views/dataScreen/index"))),
+		meta: {
+			requiresAuth: true,
+			title: "数据大屏",
+			key: "dataScreen"
+		}
 	}
 ];
 
